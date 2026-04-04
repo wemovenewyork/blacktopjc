c3ee817875a9f3117ae45f243acc314aac111a9e
wemovenewyork <wemovenewyork.net@gmail.com>
2026-04-04 17:08:04 -0400

    fix: alias react-native to react-native-web for web builds

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,13 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
+/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+
+// Alias react-native to react-native-web for web builds
+config.resolver.extraNodeModules = {
+  ...config.resolver.extraNodeModules,
+  'react-native': path.resolve(__dirname, 'node_modules/react-native-web'),
+};
 
 module.exports = config;
