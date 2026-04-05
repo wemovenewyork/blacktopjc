b4c8d940c7fc0e97213de5e2569ca9b259537d8a
wemovenewyork <wemovenewyork.net@gmail.com>
2026-04-05 01:28:42 -0400

    debug: yellow border to confirm web file is loading

--- src/screens/home/HomeScreen.web.tsx
+++ src/screens/home/HomeScreen.web.tsx
@@ -324,6 +324,8 @@ const styles = StyleSheet.create({
     paddingHorizontal: Spacing.md,
     paddingTop: Spacing.sm,
     paddingBottom: Spacing.lg,
+    borderWidth: 3,
+    borderColor: 'yellow',
   },
   heroEyebrow: {
     fontFamily: 'RobotoCondensed_400Regular',
